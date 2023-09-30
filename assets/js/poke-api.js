4e3d24e1ee9aca23f93e4f1fbdf26940b332dc64
RibamarLopes <jora_777@hotmail.com>
2023-09-30 19:40:45 -0300

    Adição das modificações feitas por mim para melhorar o código

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,4 +1,3 @@
-
 const pokeApi = {}
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
@@ -12,7 +11,7 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     pokemon.types = types
     pokemon.type = type
 
-    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+    pokemon.photo = pokeDetail.sprites.other['official-artwork'].front_default
 
     return pokemon
 }
@@ -23,7 +22,7 @@ pokeApi.getPokemonDetail = (pokemon) => {
         .then(convertPokeApiDetailToPokemon)
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
+pokeApi.getPokemons = (offset, limit) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
     return fetch(url)
